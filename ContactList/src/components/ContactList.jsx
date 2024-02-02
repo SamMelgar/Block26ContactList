06af7ad7477e4d4d30d3f94038bd5ae91efefcbe
SamMelgar <samsama.melgar@gmail.com>
2024-02-02 12:51:15 -0500

    link API to web app

--- ContactList/src/components/ContactList.jsx
+++ ContactList/src/components/ContactList.jsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ContactRow from "./ContactRow";
 
-const dummyContacts = [
-  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-];
 const ContactList = () => {
-  const [contacts, setContacts] = useState(dummyContacts);
+  const [contacts, setContacts] = useState([]);
 
+  useEffect(()=>{
+    async function fetchContacts() {
+        try {
+            // Fetch data from the API
+            const response = await fetch('https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users');
+             // Parse the response to JSON
+            const data = await response.json();
+             // Log the data to the console for testing
+            console.log('API Data:', data);
+            setContacts(data);
+        } 
+          catch (error) {
+            // Log any errors to the console
+            console.error('Error fetching contacts:', error);
+          }
+        }
+        fetchContacts();
+
+  },[])
   return (
     <table>
       <thead>
